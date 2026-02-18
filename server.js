8eda7941a6a0585eb0c8ee697e66d3e2c949038f
Anand12082001 <Anandjayakumardae@gmail.com>
2026-02-18 09:30:46 +0530

    Backend ready for render deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
 
+
 dotenv.config();
 
 connectDB();
@@ -13,14 +14,23 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://password-reset-backend-1-f3se.onrender.com"
+  ],
+  credentials: true
+}));
+const PORT = process.env.PORT || 3000;
+
 
 app.use("/api/auth", authRoutes);
 
 // Error Middleware (must be last)
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on port ${PORT}`)
-);
+
